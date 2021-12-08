9fa4e891b586f21d1e20161ae521c021b92730ae
steph45607 <staniswinata.stephanie@gmail.com>
2021-12-08 21:42:07 +0700

    Done :)

---
+++ js2.js
@@ -0,0 +1,49 @@
+function numbers(numArray, key){
+    let lenArr = numArray.length;
+
+    let changeToArray = key => Number(key);
+    const keyArr = Array.from(String(key),changeToArray);
+    let lenKey = keyArr.length;
+
+    const letters = [];
+    
+    rep = Math.floor(lenArr / lenKey);
+    res = lenArr % lenKey;
+
+
+    if(lenArr <= lenKey){
+        for(var i = 0 ; i < lenArr; i++){
+            var letter = numArray[i] - keyArr[i];
+            letters.push(letter);
+        }
+        return letters;
+    }
+    else{
+        var j = 0;
+        while(j < lenArr){
+            for(var k = 0; k < rep; k++){
+                for(var l = 0; l < lenKey; l++){
+                    var letter = numArray[j] - keyArr[l];
+                    letters.push(letter);
+                    j++;
+                }
+            }
+            // Idk how to get the for loop above to this loop below, they dont "connect" ? 
+            for(var m = 0; m < res; m++){
+                var letter = numArray[j] - keyArr[m];
+                letters.push(letter);
+                j++;
+            }
+        }
+    }
+    
+    const word = [];
+    for(var i = 0; i < letters.length; i++){
+        let char = String.fromCharCode(letters[i]+96);
+        word.push(char);
+    }
+    var result = word.join("")
+    return result;
+};
+ 
+console.log(numbers([20,12,18,30,21],1939))
